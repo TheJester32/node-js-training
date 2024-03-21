42a8472471c379ad9bfebd171b72f64f699dcf28
Bogdan Popov <114678431+TheJester32@users.noreply.github.com>
2024-03-21 13:59:28 +0300

    MongoDB
    MongoDB

---
+++ test/homework.js
@@ -0,0 +1,9 @@
+// const { expect } = require('chai')
+// const { sum } = require('./helpers')
+
+
+// describe.only('Урок 3.3', () => {
+//   it('Функция sum возвращает сумму аргументов', () => {
+//     expect(sum(1, 2, 3)).to.equal.apply(6)
+//   })
+// })
